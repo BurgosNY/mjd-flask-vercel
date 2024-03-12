b6335e256e872b148e70ee32b86391ae01c418de
BurgosNY <pedroburgos@gmail.com>
2024-03-11 21:14:50 -0300

    Mudando Index.

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Fala Galera!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():

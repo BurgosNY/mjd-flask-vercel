12d5ef05182295253fa7cf20d7d4f1b13c1b2169
BurgosNY <pedroburgos@gmail.com>
2024-03-18 21:19:22 -0300

    Index com turma

--- api/index.py
+++ api/index.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 # Rota 1
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', turma=turma)
 
 
 @app.route('/busca', methods=['GET', 'POST'])
 def busca():
-    return 'Busca'
+    numero = request.form['nome'] # request.form.get("nome")
+    for aluno in turma:
+        if aluno["id"] == numero:
+            resposta = aluno
+            break
+    return render_template('estudante.html', estudante=resposta)
 
 
 @app.route('/about')
@@ -23,8 +28,8 @@ def estudante(id):
     return render_template('estudante.html', estudante=aluno)
 
     
-turma = {"1": {"nome": "João", "idade": 20, "curso": "Engenharia"},
-         "2": {"nome": "Maria", "idade": 22, "curso": "Medicina"},
-         "3": {"nome": "José", "idade": 21, "curso": "Direito"},
-         "4": {"nome": "Ana", "idade": 23, "curso": "Administração"},
-         "5": {"nome": "Pedro", "idade": 24, "curso": "Contabilidade"}}
+turma = [{"id": "1", "nome": "João", "idade": 20, "curso": "Engenharia"},
+         {"id": "2", "nome": "Maria", "idade": 22, "curso": "Medicina"},
+         {"id": "3", "nome": "José", "idade": 21, "curso": "Direito"},
+         {"id": "4", "nome": "Ana", "idade": 23, "curso": "Administração"},
+         {"id": "5", "nome": "Pedro", "idade": 24, "curso": "Contabilidade"}]
